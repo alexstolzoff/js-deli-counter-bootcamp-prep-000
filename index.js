1280229cbb6bdbe12afb8b2da556069253aebef7
Alex Stolzoff <alexstolznut@hotmail.com>
2017-08-28 06:06:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function nowServing(array){
     // var message = `Currently serving ${person}`;
     // var elseMessage = `Currently serving ${person}`;
       return `Currently serving ${person}.`;
-      array.pop();
+      array.shift();
 
   }
 
