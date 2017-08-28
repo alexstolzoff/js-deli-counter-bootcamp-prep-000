ac0bc78d00d5ed788d2523299be19187c79a08e6
Alex Stolzoff <alexstolznut@hotmail.com>
2017-08-28 05:45:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,9 @@ function takeANumber(array, name){
   var newArray = katzDeliLine;
   newArray.push(name);
   var place = newArray.indexOf(name);
+  var spot = place+1;
+  var message = `Welcome, ${name}. You are number ${spot} in line.`
   return place+1;
 }
+
+takeANumber(katzDeliLine, "Ada");
