09df30e6398b610c662b3d33c0d75016f754ba00
Alex Stolzoff <alexstolznut@hotmail.com>
2017-08-28 06:15:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ function takeANumber(array, name){
 //   }
 //   return array;
 
-}
+// }
 takeANumber(katzDeliLine, "Ada"); // "Welcome, Ada. You are number 1 in line."
 takeANumber(katzDeliLine, "Grace"); // "Welcome, Grace. You are number 2 in line."
 takeANumber(katzDeliLine, "Kent"); // "Welcome, Kent. You are number 3 in line."
