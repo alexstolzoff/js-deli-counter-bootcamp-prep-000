75c094b0b695ec1d3356e40137981284fe04b095
Alex Stolzoff <alexstolznut@hotmail.com>
2017-08-28 06:04:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,4 +29,4 @@ function nowServing(array){
 takeANumber(katzDeliLine, "Ada"); // "Welcome, Ada. You are number 1 in line."
 takeANumber(katzDeliLine, "Grace"); // "Welcome, Grace. You are number 2 in line."
 takeANumber(katzDeliLine, "Kent"); // "Welcome, Kent. You are number 3 in line."
-nowServing(katzDeliLine);
+nowServing(newDeli);
