c3dad8604339dcf1d41d8bd7f7fb494165463e36
Alex Stolzoff <alexstolznut@hotmail.com>
2017-08-28 07:13:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 var katzDeliLine = [];
 var newDeli = ["Steven", "Blake", "Avi"];
 
-// function takeANumber(array, name){
-//   var newArray = array;
-//   newArray.push(name);
-//   var place = newArray.indexOf(name);
-//   var spot = place+1;
-//   var message = `Welcome, ${name}. You are number ${spot} in line.`;
-//   return message;
-// }
+function takeANumber(array, name){
+  var newArray = array;
+  newArray.push(name);
+  var place = newArray.indexOf(name);
+  var spot = place+1;
+  var message = `Welcome, ${name}. You are number ${spot} in line.`;
+  return message;
+}
 
 function nowServing(array){
 
@@ -33,15 +33,15 @@ function nowServing(array){
 
 }
 
-function currentLine(array){
-  if(array.length>0){
-  for (let i = 0; array.length>i; i++){
-    var person = array[i];
-    return`${i}. ${person}`;
-  }
-}
-}
-// takeANumber(katzDeliLine, "Ada"); // "Welcome, Ada. You are number 1 in line."
-// takeANumber(katzDeliLine, "Grace"); // "Welcome, Grace. You are number 2 in line."
-// takeANumber(katzDeliLine, "Kent"); // "Welcome, Kent. You are number 3 in line."
+// function currentLine(array){
+//   if(array.length>0){
+//   for (let i = 0; array.length>i; i++){
+//     var person = array[i];
+//     return`${i}. ${person}`;
+//   }
+// }
+// }
+takeANumber(katzDeliLine, "Ada"); // "Welcome, Ada. You are number 1 in line."
+takeANumber(katzDeliLine, "Grace"); // "Welcome, Grace. You are number 2 in line."
+takeANumber(katzDeliLine, "Kent"); // "Welcome, Kent. You are number 3 in line."
 nowServing(newDeli);
