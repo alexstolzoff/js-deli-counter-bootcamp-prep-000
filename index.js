e8361bfb83cfdafbc2c831995a2d0cc045648dd6
Alex Stolzoff <alexstolznut@hotmail.com>
2017-08-28 05:43:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,28 +1,8 @@
-var katzDeli = [];
 var katzDeliLine = [];
 
-function takeANumber(katzDeliLine, name){
-  katzDeliLine.push(name);
-  var spot = katzDeliLine.indexOf("name") +1;
-  var t = spot+1;
-  var message = `Welcome, ${name}. You are number ${t} in line.`
-  // return spot;
-  // return katzDeliLine;
-  return message;
+function takeANumber(array, name){
+  var newArray = katzDeliLine;
+  newArray.push(name);
+  var place = newArray.indexOf(name);
+  return place+1;
 }
-
-function nowServing(katzDeliLine){
-  var message = `Currently serving ${katzDeliLine[0]}.`
-  if(message == "Currently serving undefined"){
-    return "There is nobody waiting to be served!"
-
-  }else{
-    return message;
-    katzDeliLine.shift();
-  }
-}
-
-takeANumber(katzDeliLine, "Ada"); // "Welcome, Ada. You are number 1 in line."
-takeANumber(katzDeliLine, "Grace"); // "Welcome, Grace. You are number 2 in line."
-takeANumber(katzDeliLine, "Kent");
- nowServing(katzDeliLine);
